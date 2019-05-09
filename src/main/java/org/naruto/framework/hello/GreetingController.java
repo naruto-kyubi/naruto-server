ca2865598abc0715c8d0f57c4d9d3631e239c973
qteamcheng <64435716@qq.com>
2019-05-09 10:26:14 +0800

    删除无用引用

--- src/main/java/org/naruto/framework/hello/GreetingController.java
+++ src/main/java/org/naruto/framework/hello/GreetingController.java
@@ -1,13 +1,5 @@
 package org.naruto.framework.hello;
 
-import com.aliyuncs.CommonRequest;
-import com.aliyuncs.CommonResponse;
-import com.aliyuncs.DefaultAcsClient;
-import com.aliyuncs.IAcsClient;
-import com.aliyuncs.exceptions.ClientException;
-import com.aliyuncs.exceptions.ServerException;
-import com.aliyuncs.http.MethodType;
-import com.aliyuncs.profile.DefaultProfile;
 import lombok.extern.slf4j.Slf4j;
 import org.naruto.framework.core.exception.EmServiceError;
 import org.naruto.framework.core.exception.ServiceException;
