12a8e81f486d8e249fe0a7152b89e2772616531b
liuhy <25436136@qq.com>
2019-05-15 12:19:27 +0800

    refactor shiro login and encrpyt

---
+++ src/main/java/org/naruto/framework/core/encrpyt/DefaltEncrpyt.java
@@ -0,0 +1,15 @@
+package org.naruto.framework.core.encrpyt;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.stereotype.Component;
+import org.springframework.util.DigestUtils;
+
+@Component
+@ConditionalOnMissingBean(name="encrpytService")
+public class DefaltEncrpyt implements IEncrpyt{
+
+    @Override
+    public String encrpyt(String str, String salt) {
+        return DigestUtils.md5DigestAsHex(str.getBytes());
+    }
+}
