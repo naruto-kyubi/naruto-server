91e700c8841f5b4d52c35d7ddb2ca6dfeb49570b
liuhy <25436136@qq.com>
2019-05-17 12:16:47 +0800

    refactor authorizingRealm register

--- src/main/java/org/naruto/framework/security/ShiroConfig.java
+++ src/main/java/org/naruto/framework/security/ShiroConfig.java
@@ -1,24 +1,19 @@
 package org.naruto.framework.security;
 
+import org.apache.shiro.realm.Realm;
 import org.apache.shiro.spring.web.config.DefaultShiroFilterChainDefinition;
 import org.apache.shiro.spring.web.config.ShiroFilterChainDefinition;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
-import org.naruto.framework.security.service.captcha.CaptchaRealm;
-import org.naruto.framework.security.service.account.UserPasswordRealm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class ShiroConfig {
-    @Autowired
-    private UserPasswordRealm userPasswordRealm;
 
     @Autowired
-    private CaptchaRealm captchaRealm;
-
+    private List<Realm> authorizingRealmList;
 //    @Bean
 //    public Authenticator authenticator() {
 //        ModularRealmAuthenticator authenticator = new ModularRealmAuthenticator();
@@ -29,8 +24,9 @@ public class ShiroConfig {
 
     @Bean
     public DefaultWebSecurityManager securityManager() {
+
         DefaultWebSecurityManager  securityManager = new DefaultWebSecurityManager ();
-        securityManager.setRealms(Arrays.asList(userPasswordRealm,captchaRealm));
+        securityManager.setRealms(authorizingRealmList);
         return securityManager;
     }
 
