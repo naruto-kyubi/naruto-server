61217112e6ebcee64761e8f842a9542615b862c1
qteamcheng <64435716@qq.com>
2019-06-11 15:15:17 +0800

    add article module

---
+++ src/main/java/org/naruto/framework/catalog/repository/CatalogRepository.java
@@ -0,0 +1,8 @@
+package org.naruto.framework.catalog.repository;
+
+import org.naruto.framework.article.domain.Article;
+import org.naruto.framework.catalog.domain.Catalog;
+import org.naruto.framework.core.repository.CustomRepository;
+
+public interface CatalogRepository extends CustomRepository<Catalog,String> {
+}
