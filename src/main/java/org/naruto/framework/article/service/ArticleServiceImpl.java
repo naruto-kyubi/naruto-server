48807db05693560fa69c68ab3889d82539765bab
liuhy <25436136@qq.com>
2019-06-17 16:16:15 +0800

    new add like/star function.

---
+++ src/main/java/org/naruto/framework/article/service/ArticleServiceImpl.java
@@ -0,0 +1,98 @@
+package org.naruto.framework.article.service;
+
+import org.naruto.framework.article.domain.Article;
+import org.naruto.framework.article.domain.Comment;
+import org.naruto.framework.article.domain.Like;
+import org.naruto.framework.article.domain.Star;
+import org.naruto.framework.article.repository.ArticleRepository;
+import org.naruto.framework.article.repository.CommentRepository;
+import org.naruto.framework.article.repository.LikeRepository;
+import org.naruto.framework.article.repository.StarRepository;
+import org.naruto.framework.core.exception.CommonError;
+import org.naruto.framework.core.exception.ServiceException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Map;
+
+@Service
+public class ArticleServiceImpl implements ArticleService {
+
+    @Autowired
+    private ArticleRepository articleRepository;
+
+    @Autowired
+    private CommentRepository commentRepository;
+
+    @Autowired
+    private LikeRepository likeRepository;
+
+    @Autowired
+    private StarRepository starRepository;
+
+    public Article saveArticle(Article article){
+        if(article == null) {
+                throw new ServiceException(CommonError.PARAMETER_VALIDATION_ERROR);
+        }
+
+        return articleRepository.save(article);
+    }
+
+
+    public Page<Article> queryArticleByPage(Map map) {
+        return articleRepository.queryPageByCondition(map);
+    }
+
+    public Article queryArticleById(String id){
+        return  articleRepository.findById(id).get();
+    }
+
+    public Comment saveComment(Comment comment){
+        if(comment == null) {
+            throw new ServiceException(CommonError.PARAMETER_VALIDATION_ERROR);
+        }
+
+        return commentRepository.save(comment);
+    }
+
+    public Page<Comment> queryCommentByPage(Map map) {
+        return commentRepository.queryPageByCondition(map);
+    }
+
+    //like,zhan;
+
+    public Like queryLikeByUserIdAndTypeAndTargetId(String userId,String type,String targetId){
+
+        return likeRepository.queryLikeByUserIdAndTypeAndTargetId(userId,type,targetId);
+    }
+
+    public Like saveLike(Like like){
+        return likeRepository.save(like);
+    }
+
+    @Transactional
+    public void deleteLike(String userId,String type,String targetId){
+        likeRepository.deleteLikeByUserIdAndTypeAndTargetId(userId,type,targetId);
+    }
+
+    @Override
+    public Star queryStarByUserIdAndArticleId(String userId, String articleId) {
+
+        return starRepository.queryStarByUserIdAndArticleId(userId,articleId);
+    }
+
+    @Override
+    public Star saveStar(Star star) {
+
+        return starRepository.save(star);
+    }
+
+    @Transactional
+    @Override
+    public void deleteStar(String userId, String articleId) {
+
+        starRepository.deleteByUserIdAndArticleId(userId,articleId);
+    }
+}
