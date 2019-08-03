9b02ebe87f57056f286e444433cb7c9e66f353a4
qteamcheng <64435716@qq.com>
2019-08-03 19:02:40 +0800

    WebMvcConfigurerSupport 不能排除静态资源，改为实现WebMvcConfigurer接口。

--- src/main/java/org/naruto/framework/WebAppConfigurer.java
+++ src/main/java/org/naruto/framework/WebAppConfigurer.java
@@ -2,12 +2,15 @@ package org.naruto.framework;
 
 import org.naruto.framework.article.ArticleReadedStatInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebAppConfigurer extends WebMvcConfigurationSupport {
+@ComponentScan(value="org.naruto.framework.article")
+public class WebAppConfigurer implements WebMvcConfigurer {
     @Autowired
     private ArticleReadedStatInterceptor articleReadedStatInterceptor;
     @Override
@@ -16,11 +19,11 @@ public class WebAppConfigurer extends WebMvcConfigurationSupport {
         //多个拦截器组成一个拦截器链
         //addPathPatterns 用于添加拦截规则
         //excludePathPatterns用于排除拦截规则
-        interceptorRegistry.addInterceptor(articleReadedStatInterceptor).addPathPatterns("/v1/articles/*");
+        interceptorRegistry.addInterceptor(articleReadedStatInterceptor).addPathPatterns("/v1/articles/*").excludePathPatterns("/images/*");
         //设置（模糊）匹配的url
         //    List<String> urlPatterns = Lists.newArrayList();
         //    urlPatterns.add("/api/v1/goods/*");
         //    urlPatterns.add("/api/v1/userinfo/*");
-        super.addInterceptors(interceptorRegistry);
+       // super.addInterceptors(interceptorRegistry);
     }
 }
